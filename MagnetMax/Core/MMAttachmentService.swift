a0bf19dca9a1cbd7789d54aa3744f6c9e2a9a912
Pritesh Shah <pritesh.shah@magnet.com>
2015-11-19 21:12:00 -0800

    Attachments can now be jsonified

--- MagnetMax/Core/MMAttachmentService.swift
+++ MagnetMax/Core/MMAttachmentService.swift
@@ -17,42 +17,141 @@
 
 import Foundation
 import AFNetworking
+import Mantle
+
 
 @objc public class MMAttachment: NSObject {
     public private(set) var attachmentID: String?
-    public private(set) var name: String
-    public private(set) var summary: String
+    public private(set) var name: String?
+    public private(set) var summary: String?
     public private(set) var mimeType: String
     
-    internal init(name: String, summary: String, mimeType: String) {
+    init(name: String?, summary: String?, mimeType: String) {
         self.name = name
         self.summary = summary
         self.mimeType = mimeType
     }
+    
+    func toDictionary() -> [String: AnyObject] {
+        var dictionary = ["mimeType": mimeType]
+        if let attachmentID = self.attachmentID {
+            dictionary["attachmentID"] = attachmentID
+        }
+        if let name = self.name {
+            dictionary["name"] = name
+        }
+        if let summary = self.summary {
+            dictionary["summary"] = summary
+        }
+        
+        return dictionary
+    }
+    
+    func toJSON() -> NSData? {
+        let dictionary = toDictionary()
+        if NSJSONSerialization.isValidJSONObject(dictionary) {
+            do {
+                let data = try NSJSONSerialization.dataWithJSONObject(dictionary, options: NSJSONWritingOptions(rawValue: 0))
+                return data
+            } catch {
+                return nil
+            }
+        }
+        return nil
+    }
+    
+    public func toJSONString() -> String {
+        var jsonString = ""
+        if let jsonData = toJSON() {
+            jsonString = NSString(data: jsonData, encoding: NSUTF8StringEncoding) as! String
+        }
+        
+        return jsonString
+    }
+    
+    static public func fromJSONString(jsonString: String) -> Self? {
+        do {
+            let object = try NSJSONSerialization.JSONObjectWithData(jsonString.dataUsingEncoding(NSUTF8StringEncoding)!, options: NSJSONReadingOptions(rawValue: 0))
+            if let dictionary = object as? [String: AnyObject] {
+                return fromDictionary(dictionary)
+            }
+        } catch {
+            return nil
+        }
+        
+        return nil
+    }
+    
+    class func fromDictionary(dictionary: [String: AnyObject]) -> Self? {
+        return nil
+    }
 }
 
 @objc public class MMFileAttachment: MMAttachment {
-    public private(set) var fileURL: NSURL
-    public private(set) var fileName: String
+    public private(set) var fileURL: NSURL?
+    public private(set) var fileName: String?
     
-    public init(fileURL: NSURL, fileName: String, name: String, summary: String, mimeType: String) {
+    required public init(fileURL: NSURL?, fileName: String?, name: String?, summary: String?, mimeType: String) {
         self.fileName = fileName
         self.fileURL = fileURL
         super.init(name: name, summary: fileName, mimeType: mimeType)
     }
     
+    override public func toDictionary() -> [String: AnyObject] {
+        var dictionary = super.toDictionary()
+        if let fileName = self.fileName {
+            dictionary["fileName"] = fileName
+        }
+        
+        return dictionary
+    }
+    
+    override class func fromDictionary(dictionary: [String: AnyObject]) -> Self? {
+        let fileAttachment = self.init(fileURL: nil, fileName: dictionary["fileName"] as? String, name: dictionary["name"] as? String, summary: dictionary["summary"] as? String, mimeType: dictionary["mimeType"] as! String)
+        fileAttachment.attachmentID = dictionary["attachmentID"] as? String
+        
+        return fileAttachment
+    }
+    
+    public func download(success: ((attachment: MMFileAttachment) -> Void)?, failure: ((error: NSError) -> Void)?) {
+        // FIXME: Check for attachmentID
+        if let attachmentID = self.attachmentID {
+            MMAttachmentService.download(attachmentID, success: { URL in
+                self.fileURL = URL
+                success?(attachment: self)
+                }) { error in
+                    failure?(error: error)
+            }
+        }
+    }
+    
 }
 
 @objc public class MMDataAttachment: MMAttachment {
-    public private(set) var data: NSData
-    public private(set) var fileName: String
+    public private(set) var data: NSData?
+    public private(set) var fileName: String?
     
-    init(data: NSData, fileName: String, name: String, summary: String, mimeType: String) {
+    required public init(data: NSData?, fileName: String?, name: String?, summary: String?, mimeType: String) {
         self.data = data
         self.fileName = fileName
         super.init(name: name, summary: fileName, mimeType: mimeType)
     }
     
+    override public func toDictionary() -> [String: AnyObject] {
+        var dictionary = super.toDictionary()
+        if let fileName = self.fileName {
+            dictionary["fileName"] = fileName
+        }
+        
+        return dictionary
+    }
+    
+    override class func fromDictionary(dictionary: [String: AnyObject]) -> Self? {
+        let dataAttachment = self.init(data: nil, fileName: dictionary["fileName"] as? String, name: dictionary["name"] as? String, summary: dictionary["summary"] as? String, mimeType: dictionary["mimeType"] as! String)
+        dataAttachment.attachmentID = dictionary["attachmentID"] as? String
+        
+        return dataAttachment
+    }
 }
 
 @objc public class MMAttachmentService: NSObject {
@@ -64,14 +163,14 @@ import AFNetworking
         let request = AFHTTPRequestSerializer().multipartFormRequestWithMethod(MMStringFromRequestMethod(MMRequestMethod.POST), URLString: uploadURL, parameters: nil, constructingBodyWithBlock: { formData in
             for attachment in attachments {
                 if let fileAttachment = attachment as? MMFileAttachment {
-                    let _ = try? formData.appendPartWithFileURL(fileAttachment.fileURL, name: fileAttachment.name, fileName: fileAttachment.fileName, mimeType: fileAttachment.mimeType)
+                    let _ = try? formData.appendPartWithFileURL(fileAttachment.fileURL!, name: fileAttachment.name ?? "file", fileName: fileAttachment.fileName ?? fileAttachment.fileURL?.lastPathComponent ?? "unknownFile", mimeType: fileAttachment.mimeType)
                 } else if let dataAttachment = attachment as? MMDataAttachment {
-                    formData.appendPartWithFileData(dataAttachment.data, name: dataAttachment.name, fileName: dataAttachment.fileName, mimeType: dataAttachment.mimeType)
+                    formData.appendPartWithFileData(dataAttachment.data!, name: dataAttachment.name ?? "file", fileName: dataAttachment.fileName ?? "unknownFile", mimeType: dataAttachment.mimeType)
                 } else {
                     
                 }
             }
-        }, error: nil)
+            }, error: nil)
         
         request.setValue("Bearer \(MMCoreConfiguration.serviceAdapter.HATToken)", forHTTPHeaderField: "Authorization")
         let uploadTask = MMCoreConfiguration.serviceAdapter.backgroundSessionManager.uploadTaskWithStreamedRequest(request, progress: nil) { response, responseObject, error in
@@ -89,7 +188,7 @@ import AFNetworking
         uploadTask.resume()
     }
     
-    static public func download(attachmentID: String, success: ((MMAttachment) -> ())?, failure: ((error: NSError) -> Void)?) {
+    static public func download(attachmentID: String, success: ((NSURL) -> ())?, failure: ((error: NSError) -> Void)?) {
         guard let downloadURL = NSURL(string: "com.magnet.server/file/download/\(attachmentID)", relativeToURL: MMCoreConfiguration.serviceAdapter.endPoint.URL) else {
             fatalError("downloadURL should not be nil")
         }
@@ -103,9 +202,16 @@ import AFNetworking
             if let e = error {
                 failure?(error: e)
             } else {
-                let fileAttachment = MMFileAttachment(fileURL: filePath!, fileName: "", name: "", summary: "", mimeType: "image/jpeg")
-                success?(fileAttachment)
-            }
+//                guard let httpResponse = response as? NSHTTPURLResponse else {
+//                    fatalError("response should be of type NSHTTPURLResponse")
+//                }
+//                let headers = httpResponse.allHeaderFields["Content-Type"] as! [String: AnyObject]
+//                var contentType = "application/octet-stream"
+//                if let contentTypeHeader = headers["Content-Type"] as? String {
+//                    contentType = contentTypeHeader
+//                }
+                    success?(filePath!)
+                }
         }
         
         downloadTask.resume()
