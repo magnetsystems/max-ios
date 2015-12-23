eb820b374d1cec4e1e5d383b48ce52ef614820b5
Lorenzo Stanton <lorenzo.stanton@Lorenzo-Stantons-MacBook-Pro.local>
2015-12-22 16:54:00 -0800

    **Reverted formatting **Implemented MMUserDelegate in MagnetMax

--- MagnetMax/Core/MMUser.swift
+++ MagnetMax/Core/MMUser.swift
@@ -1,19 +1,19 @@
 /*
-* Copyright (c) 2015 Magnet Systems, Inc.
-* All rights reserved.
-*
-* Licensed under the Apache License, Version 2.0 (the "License"); you
-* may not use this file except in compliance with the License. You
-* may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
-* implied. See the License for the specific language governing
-* permissions and limitations under the License.
-*/
+ * Copyright (c) 2015 Magnet Systems, Inc.
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You
+ * may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
 
 import Foundation
 
@@ -30,43 +30,43 @@ public extension MMUser {
     @nonobjc static public var delegate : MMUserDelegate.Type?
     
     /**
-     Registers a new user.
+        Registers a new user.
      
-     - Parameters:
-     - success: A block object to be executed when the registration finishes successfully. This block has no return value and takes one argument: the newly created user.
-     - failure: A block object to be executed when the registration finishes with an error. This block has no return value and takes one argument: the error object.
-     */
+        - Parameters:
+            - success: A block object to be executed when the registration finishes successfully. This block has no return value and takes one argument: the newly created user.
+            - failure: A block object to be executed when the registration finishes with an error. This block has no return value and takes one argument: the error object.
+    */
     public func register(success: ((user: MMUser) -> Void)?, failure: ((error: NSError) -> Void)?) {
-        
+
         assert(!userName.isEmpty && !password.isEmpty, "userName or password cannot be empty")
         
         MMCoreConfiguration.serviceAdapter.registerUser(self, success: { user in
             success?(user: user)
-            }) { error in
-                failure?(error: error)
-            }.executeInBackground(nil)
+        }) { error in
+            failure?(error: error)
+        }.executeInBackground(nil)
     }
     
     /**
-     Logs in as an user.
+        Logs in as an user.
      
-     - Parameters:
-     - credential: A credential object containing the user's userName and password.
-     - success: A block object to be executed when the login finishes successfully. This block has no return value and takes no arguments.
-     - failure: A block object to be executed when the login finishes with an error. This block has no return value and takes one argument: the error object.
-     */
+        - Parameters:
+            - credential: A credential object containing the user's userName and password.
+            - success: A block object to be executed when the login finishes successfully. This block has no return value and takes no arguments.
+            - failure: A block object to be executed when the login finishes with an error. This block has no return value and takes one argument: the error object.
+    */
     static public func login(credential: NSURLCredential, success: (() -> Void)?, failure: ((error: NSError) -> Void)?) {
         login(credential, rememberMe: false, success: success, failure: failure)
     }
     
     /**
-     Logs in as an user.
+        Logs in as an user.
      
-     - Parameters:
-     - credential: A credential object containing the user's userName and password.
-     - rememberMe: A boolean indicating if the user should stay logged in across app restarts.
-     - success: A block object to be executed when the login finishes successfully. This block has no return value and takes no arguments.
-     - failure: A block object to be executed when the login finishes with an error. This block has no return value and takes one argument: the error object.
+        - Parameters:
+            - credential: A credential object containing the user's userName and password.
+            - rememberMe: A boolean indicating if the user should stay logged in across app restarts.
+            - success: A block object to be executed when the login finishes successfully. This block has no return value and takes no arguments.
+            - failure: A block object to be executed when the login finishes with an error. This block has no return value and takes one argument: the error object.
      */
     static public func login(credential: NSURLCredential, rememberMe: Bool, success: (() -> Void)?, failure: ((error: NSError) -> Void)?) {
         MMCoreConfiguration.serviceAdapter.loginWithUsername(credential.user, password: credential.password, rememberMe: rememberMe, success: { _ in
@@ -115,11 +115,12 @@ public extension MMUser {
             }, error: error, context: context)
     }
     
+    
     /**
-     Acts as the userToken expired event receiver.
+        Acts as the userToken expired event receiver.
      
-     - Parameters:
-     - notification: The notification that was received.
+        - Parameters:
+            - notification: The notification that was received.
      */
     @objc static private func userTokenExpired(notification: NSNotification?) {
         currentlyLoggedInUser = nil
@@ -128,18 +129,18 @@ public extension MMUser {
     }
     
     /**
-     Logs out a currently logged-in user.
-     */
+        Logs out a currently logged-in user.
+    */
     static public func logout() {
         logout(nil, failure: nil)
     }
     /**
-     Logs out a currently logged-in user.
+        Logs out a currently logged-in user.
      
-     - Parameters:
-     - success: A block object to be executed when the logout finishes successfully. This block has no return value and takes no arguments.
-     - failure: A block object to be executed when the logout finishes with an error. This block has no return value and takes one argument: the error object.
-     */
+        - Parameters:
+            - success: A block object to be executed when the logout finishes successfully. This block has no return value and takes no arguments.
+            - failure: A block object to be executed when the logout finishes with an error. This block has no return value and takes one argument: the error object.
+    */
     static public func logout(success: (() -> Void)?, failure: ((error: NSError) -> Void)?) {
         if currentUser() == nil {
             success?()
@@ -150,67 +151,67 @@ public extension MMUser {
         
         MMCoreConfiguration.serviceAdapter.logoutWithSuccess({ _ in
             success?()
-            }) { error in
-                failure?(error: error)
-            }.executeInBackground(nil)
+        }) { error in
+            failure?(error: error)
+        }.executeInBackground(nil)
     }
     
     /**
-     Get the currently logged-in user.
+        Get the currently logged-in user.
      
-     - Returns: The currently logged-in user or nil.
-     */
+        - Returns: The currently logged-in user or nil.
+    */
     static public func currentUser() -> MMUser? {
         return currentlyLoggedInUser
     }
     
     /**
-     Search for users based on some criteria.
+        Search for users based on some criteria.
      
-     - Parameters:
-     - query: The DSL can be found here: https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-query-string-query.html#query-string-syntax
-     - limit: The number of records to retrieve.
-     - offset: The offset to start from.
-     - sort: The sort criteria.
-     - success: A block object to be executed when the call finishes successfully. This block has no return value and takes one argument: the list of users that match the specified criteria.
-     - failure: A block object to be executed when the call finishes with an error. This block has no return value and takes one argument: the error object.
-     */
+        - Parameters:
+            - query: The DSL can be found here: https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-query-string-query.html#query-string-syntax
+            - limit: The number of records to retrieve.
+            - offset: The offset to start from.
+            - sort: The sort criteria.
+            - success: A block object to be executed when the call finishes successfully. This block has no return value and takes one argument: the list of users that match the specified criteria.
+            - failure: A block object to be executed when the call finishes with an error. This block has no return value and takes one argument: the error object.
+    */
     static public func searchUsers(query: String, limit take: Int, offset skip: Int, sort: String, success: (([MMUser]) -> Void)?, failure: ((error: NSError) -> Void)?) {
         
         let userService = MMUserService()
         userService.searchUsers(query, take: Int32(take), skip: Int32(skip), sort: sort, success: { users in
             success?(users)
-            }) { error in
-                failure?(error: error)
-            }.executeInBackground(nil)
+        }) { error in
+            failure?(error: error)
+        }.executeInBackground(nil)
     }
     
     /**
-     Get users with userNames.
+        Get users with userNames.
      
-     - Parameters:
-     - userNames: A list of userNames to fetch users for.
-     - success: A block object to be executed when the logout finishes successfully. This block has no return value and takes one argument: the list of users for the specified userNames.
-     - failure: A block object to be executed when the logout finishes with an error. This block has no return value and takes one argument: the error object.
-     */
+        - Parameters:
+            - userNames: A list of userNames to fetch users for.
+            - success: A block object to be executed when the logout finishes successfully. This block has no return value and takes one argument: the list of users for the specified userNames.
+            - failure: A block object to be executed when the logout finishes with an error. This block has no return value and takes one argument: the error object.
+    */
     static public func usersWithUserNames(userNames:[String], success: (([MMUser]) -> Void)?, failure: ((error: NSError) -> Void)?) {
         
         let userService = MMUserService()
         userService.getUsersByUserNames(userNames, success: { users in
             success?(users)
-            }) { error in
-                failure?(error: error)
-            }.executeInBackground(nil)
+        }) { error in
+            failure?(error: error)
+        }.executeInBackground(nil)
     }
     
     /**
-     Get users with userIDs.
+        Get users with userIDs.
      
-     - Parameters:
-     - userNames: A list of userIDs to fetch users for.
-     - success: A block object to be executed when the logout finishes successfully. This block has no return value and takes one argument: the list of users for the specified userIDs.
-     - failure: A block object to be executed when the logout finishes with an error. This block has no return value and takes one argument: the error object.
-     */
+        - Parameters:
+            - userNames: A list of userIDs to fetch users for.
+            - success: A block object to be executed when the logout finishes successfully. This block has no return value and takes one argument: the list of users for the specified userIDs.
+            - failure: A block object to be executed when the logout finishes with an error. This block has no return value and takes one argument: the error object.
+    */
     static public func usersWithUserIDs(userIDs:[String], success: (([MMUser]) -> Void)?, failure: ((error: NSError) -> Void)?) {
         
         let userService = MMUserService()
--- MagnetMax/MagnetMax.swift
+++ MagnetMax/MagnetMax.swift
@@ -1,19 +1,19 @@
 /*
-* Copyright (c) 2015 Magnet Systems, Inc.
-* All rights reserved.
-*
-* Licensed under the Apache License, Version 2.0 (the "License"); you
-* may not use this file except in compliance with the License. You
-* may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
-* implied. See the License for the specific language governing
-* permissions and limitations under the License.
-*/
+ * Copyright (c) 2015 Magnet Systems, Inc.
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You
+ * may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
 
 import Foundation
 import MagnetMaxCore
@@ -24,12 +24,13 @@ import MMX
     
     /// The service adapter with the current configuration.
     static var serviceAdapter: MMServiceAdapter?
+    
     /**
-     Configure MagnetMax with specified configuration.
+        Configure MagnetMax with specified configuration.
      
-     - Parameters:
-     - configuration: The configuration to be used.
-     */
+        - Parameters:
+            - configuration: The configuration to be used.
+    */
     static public func configure(configuration: MMConfiguration) {
         registerObservers()
         MMCoreConfiguration.currentConfiguration = configuration
@@ -63,11 +64,11 @@ import MMX
     }
     
     /**
-     Acts as the configuration receiver.
+        Acts as the configuration receiver.
      
-     - Parameters:
-     - notification: The notification that was received.
-     */
+        - Parameters:
+            - notification: The notification that was received.
+    */
     @objc static private func configurationReceived(notification: NSNotification) {
         configuration = notification.userInfo
         for module in modules {
@@ -78,11 +79,11 @@ import MMX
     }
     
     /**
-     Acts as the appToken receiver.
+        Acts as the appToken receiver.
      
-     - Parameters:
-     - notification: The notification that was received.
-     */
+        - Parameters:
+            - notification: The notification that was received.
+    */
     @objc static private func appTokenReceived(notification: NSNotification) {
         let userInfo = notification.userInfo as! [String: String]
         appID = userInfo["appID"]
@@ -96,11 +97,11 @@ import MMX
     }
     
     /**
-     Acts as the userToken receiver.
+        Acts as the userToken receiver.
      
-     - Parameters:
-     - notification: The notification that was received.
-     */
+        - Parameters:
+            - notification: The notification that was received.
+    */
     @objc static private func userTokenReceived(notification: NSNotification) {
         let userInfo = notification.userInfo as! [String: String]
         userID = userInfo["userID"]
@@ -114,11 +115,11 @@ import MMX
     }
     
     /**
-     Acts as the userToken invalidated event receiver.
+        Acts as the userToken invalidated event receiver.
      
-     - Parameters:
-     - notification: The notification that was received.
-     */
+        - Parameters:
+            - notification: The notification that was received.
+    */
     @objc static private func userTokenInvalidated(notification: NSNotification) {
         
         if let userInfo = notification.userInfo as? [String: String] {
@@ -138,10 +139,10 @@ import MMX
     }
     
     /**
-     Acts as the userToken expired event receiver.
+        Acts as the userToken expired event receiver.
      
-     - Parameters:
-     - notification: The notification that was received.
+        - Parameters:
+            - notification: The notification that was received.
      */
     @objc static private func userTokenExpired(notification: NSNotification) {
         NSNotificationCenter.defaultCenter().postNotificationName(MMApplicationDidReceiveAuthenticationChallengeNotification, object: nil, userInfo: notification.userInfo)
@@ -149,13 +150,13 @@ import MMX
     }
     
     /**
-     Initialize a module.
+        Initialize a module.
      
-     - Parameters:
-     - module: The module to be initialized.
-     - success: A block object to be executed when the initialization finishes successfully. This block has no return value and takes no arguments.
-     - failure: A block object to be executed when the initialization finishes with an error. This block has no return value and takes one argument: the error object.
-     */
+        - Parameters:
+            - module: The module to be initialized.
+            - success: A block object to be executed when the initialization finishes successfully. This block has no return value and takes no arguments.
+            - failure: A block object to be executed when the initialization finishes with an error. This block has no return value and takes one argument: the error object.
+    */
     static public func initModule(module: MMModule, success: (() -> Void), failure: ((error: NSError) -> Void)) {
         success()
     }
@@ -169,11 +170,11 @@ import MMX
     }
     
     /**
-     Deinitialize a module.
+        Deinitialize a module.
      
-     - Parameters:
-     - module: The module to be deinitialized.
-     */
+        - Parameters:
+            - module: The module to be deinitialized.
+    */
     static private func deinitModule(module: MMModule) {
         modules = modules.filter {$0 !== module}
         module.shouldDeInitialize?()
@@ -182,7 +183,7 @@ import MMX
     /// A queue to synchronize module initialization.
     static private var moduleQueue: dispatch_queue_t = {
         return dispatch_queue_create("com.magnet.iOS.moduleQueue", nil)
-    }()
+        }()
     
     /// The current configuration.
     static private var configuration: [NSObject: AnyObject]?
@@ -216,8 +217,8 @@ import MMX
                 module?.didReceiveUserToken?(userToken!, userID: userID!, deviceID: deviceID!)
             }
             // FIXME: Don't nil out the configuration for now
-            //            success = nil
-            //            failure = nil
+//            success = nil
+//            failure = nil
         }
     }
     
